79877b4e281cc93ea95b074502cf45e5ebf32288
Connor Blakeney <connorblakeney@yahoo.com>
2020-08-05 14:53:29 -0500

    commit to merge

--- scripts/attractions/AttractionDialogButton.js
+++ scripts/attractions/AttractionDialogButton.js
@@ -1,25 +1,15 @@
-// Event to close dialog button
-
-import {useAttractions} from "./AttractionProvider"
-
 const contentTarget = document.querySelector(".previewContainer")
 const eventHub = document.querySelector(".container")
 
-contentTarget.addEventListener("click", (event) => {
-  if (event.target.id === "closeWindow") {
-    const attractionDialogBox = document.querySelector.startsWith(".details--")
-    attractionDialogBox.close()
-  }
-})
-
-eventHub.addEventListener("detailsClicked", (customEvent) => {
-  const contentTarget = document.querySelector(".attractionsDialog")
-  const attractionId = customEvent.detail.attractionChosen
-
-  const targetAttraction = useAttractions().find(
-    (attraction) => attraction.id === parseInt(attractionId)
-  )
-
-  // Show the dialog element
-  contentTarget.showModal()
-})
+export const showAttractionDetails = () => {
+  eventHub.addEventListener("click", (clickEvent) => {
+    if (clickEvent.target.id.startsWith("detailButton--")) {
+      const buttonId = clickEvent.target.id.split("--")[1]
+      const contentTarget = document.querySelector(`.attraction--${buttonId}`)
+      contentTarget.showModal()
+    } else if (clickEvent.target.id === "closeWindow") {
+      const theDialog = clickEvent.target.parentNode
+      theDialog.close()
+    }
+  })
+}
