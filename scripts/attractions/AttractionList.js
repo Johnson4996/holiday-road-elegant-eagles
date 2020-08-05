0652f431c735cd2f348a4aeb90d8f163c8327d17
Connor Blakeney <connorblakeney@yahoo.com>
2020-08-05 09:46:05 -0500

    began working on atrraction component

---
+++ scripts/attractions/AttractionList.js
@@ -0,0 +1,5 @@
+const attractionDropdown = document.getElementById("attractions")
+const contentTarget = document.querySelector(".attractions")
+
+const clearAttractionsList = () => (contentTarget.innerHTML = "")
+let attractionSelectionArray = []
---
+++ scripts/attractions/AttractionProvider.js
@@ -0,0 +1,11 @@
+let attractions = []
+
+export const getAttractions = () => {
+  return fetch("http://holidayroad.nss.team/bizarreries")
+    .then((response) => response.json())
+    .then((parsedAttractions) => {
+      attractions = parsedAttractions
+    })
+}
+
+export const useAttractions = () => attractions.slice()
