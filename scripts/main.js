6c9743f51b8c18c4ef0e304e3323afc38a7f02d3
Michael-Tyler <miketylerabc@gmail.com>
2020-08-05 15:09:50 -0500

    weather provider works as far as pulling personal keys, but not lat and long

--- scripts/main.js
+++ scripts/main.js
@@ -12,11 +12,3 @@ eateryList()
     console.log(test)
 })
 */
-import { useWeather } from "./weather/WeatherProvider.js";
-import { getWeather } from "./weather/WeatherProvider.js";
-
-
-getWeather().then(() => {
-    const weatherTest = useWeather()
-    console.log(weatherTest)
-})
--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -1,4 +1,4 @@
-//import { Settings } from "../Settings.js";
+import settings from "../Settings.js"
 
 let weather = []
 
@@ -6,11 +6,9 @@ export const useWeather = () => {
     return weather.slice()
 }
 export const getWeather = () => {
-    fetch(`api.openweathermap.org/data/2.5/forecast?lat=38.916554&lon=-77.025977&appid=c4b328627e026517ffdfa2177690f12b`)
+    return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=38&lon=-77&appid=${settings.weatherKey}`)
         .then(response => response.json())
-        .then(() => {
-            weatherData => {
-                weather = weatherData
-            }
+        .then(weatherData => {
+            weather = weatherData.list
         })
 }
