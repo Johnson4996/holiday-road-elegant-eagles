998d1931953b98ecf8a7a872c38ccc31b6de7d4b
Michael-Tyler <miketylerabc@gmail.com>
2020-08-06 12:35:23 -0500

    fixed html converter for weather

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -7,13 +7,10 @@ let weather = []
 
 eventHub.addEventListener("parkSelected", (parkSelected) => {
     const parkChosen = parkSelected.detail.parkId
-    console.log("THIS IS GRABING PARK CODE", parkChosen)
     const allTheParks = useParks()
-    console.log(allTheParks)
     const currentChosenPark = allTheParks.find(park => {
         return park.parkCode === parkChosen
     })
-    console.log(currentChosenPark)
     getWeather(currentChosenPark)
 })
 
@@ -21,7 +18,7 @@ export const useWeather = () => {
     return weather.slice()
 }
 export const getWeather = (parkObject) => {
-    return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${parkObject.latitude}&lon=${parkObject.latitude}&exclude=minutely,hourly,current&units=imperial&appid=${settings.weatherKey}`)
+    return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${parkObject.latitude}&lon=${parkObject.longitude}&exclude=minutely,hourly,current&units=imperial&appid=${settings.weatherKey}`)
         .then(response => response.json())
         .then(weatherData => {
             weather = weatherData.daily
--- scripts/weather/weatherHTML.js
+++ scripts/weather/weatherHTML.js
@@ -1,35 +1,11 @@
  export const weatherHTML = (weatherObj) => {
      return `
     <article class="weatherForecast">
-    <div class="day--">
+    <div class="day--${weatherObj.dt}">
+        <div>${weatherObj.dt}</div>
         <div>${weatherObj.weather.description}</div>
-        <div>${weatherObj.main.}</div>
-        <div>Temperature High${weatherObj.main.}</div>
-        <div>Temperature Low${weatherObj.main.}</div>
-    </div>
-    <div class="day--">
-        <div>${weatherObj}</div>
-        <div>${weatherObj}</div>
-        <div>${weatherObj}</div>
-        <div>${weatherObj}</div>
-    </div>
-    <div class="day--">
-        <div>${weatherObj}</div>
-        <div>${weatherObj}</div>
-        <div>${weatherObj}</div>
-        <div>${weatherObj}</div>
-    </div>
-    <div class="day--">
-        <div>${weatherObj}</div>
-        <div>${weatherObj}</div>
-        <div>${weatherObj}</div>
-        <div>${weatherObj}</div>
-    </div>
-    <div class="day--">
-        <div>${weatherObj}</div>
-        <div>${weatherObj}</div>
-        <div>${weatherObj}</div>
-        <div>${weatherObj}</div>
+        <div>Temperature High${weatherObj.temp.max}</div>
+        <div>Temperature Low${weatherObj.temp.min}</div>
     </div>
 </article>
     `
