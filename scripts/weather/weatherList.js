529cda5c69bd9b047239a5e303d7dfca17e4fbfe
Michael-Tyler <miketylerabc@gmail.com>
2020-08-06 14:48:09 -0500

    added div for weather flexing

--- scripts/weather/weatherList.js
+++ scripts/weather/weatherList.js
@@ -1,7 +1,7 @@
 import { weatherHTML } from "./weatherHTML.js"
 import { useWeather } from "./WeatherProvider.js"
 
-const contentTarget = document.querySelector(".park") //or ".previewContent"
+const contentTarget = document.querySelector(".weather") //or ".previewContent"
 
 export const weatherList = () => {
     contentTarget.innerHTML += `<h4>Your 5 day Forecast</h4>`
@@ -13,8 +13,7 @@ export const weatherList = () => {
 
 const render = (weatherObj) => {
     contentTarget.innerHTML += `
-    <article class="weatherForecast">
     ${weatherHTML(weatherObj)}
-    </article>
+
     `
 }
