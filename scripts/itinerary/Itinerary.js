8e63e3e15b19b8242fd06ee02848f7932cd2d10a
Connor Blakeney <connorblakeney@yahoo.com>
2020-08-10 10:52:03 -0500

    changed html for attractions and eateries to plural

--- scripts/itinerary/Itinerary.js
+++ scripts/itinerary/Itinerary.js
@@ -3,11 +3,11 @@ export const itineraryHTMLConverter = (itineraryObj) => {
         <section class="plan">
             <div class="dialogTitle">Park: </div>
             <div class="dialogContent">${itineraryObj.park}</div>
-            <div class="dialogTitle">Attraction: </div>
+            <div class="dialogTitle">Attractions: </div>
             ${itineraryObj.attraction.map(attractionObj => {
                 return `<div class="dialogContent">${attractionObj.name}</div>`
             }).join("")}
-            <div class="dialogTitle">Eatery: </div>
+            <div class="dialogTitle">Eateries: </div>
             ${itineraryObj.eatery.map(eateryObj => {
                 return `<div class="dialogContent">${eateryObj.businessName}</div>`
             }).join("")}
